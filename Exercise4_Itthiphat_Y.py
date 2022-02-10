f03c6efde64785146b20ba816195eb567af09c1c
TG714 <tg714.gamer@gmail.com>
2022-02-10 07:38:40 +0700

    Added CP3_Exercise4_Itthiphat_Y File
    First Commit.

---
+++ Exercise4_Itthiphat_Y.py
@@ -0,0 +1,10 @@
+Found_Eng = 80
+Gen_Business = 65.5
+Intro_Comp_Systems = 73.5
+Com_Program = 96
+
+print("--- Your Score ---")
+print("Foundation English :",Found_Eng, "points")
+print("General Business :",Gen_Business, "points")
+print("Introduction to Computer Systems :",Intro_Comp_Systems, "points")
+print("Computer Programming :",Com_Program, "points")
